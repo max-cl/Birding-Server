6f96d5921bd46883a05fee6da0ababbedf770cbf
max-cl <maximiliano.arias@gmail.com>
2021-11-22 13:20:37 +0000

    Add documentation dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     app.use(helmet());
+
+    const config = new DocumentBuilder()
+        .setTitle('Canary Islands Birds MobileApp')
+        .setDescription("The Canary Islands Birds API's")
+        .setVersion('1.0')
+        .addTag('Mobile App')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(process.env.PORT, () =>
         console.log(`Server listening on port: ${process.env.PORT}`),
     );
